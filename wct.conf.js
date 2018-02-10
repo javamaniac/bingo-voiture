af7d589820663b75ee82c0bb37dbb9d9b404f78c
Dominic <dominic.stack@gmail.com>
2018-02-10 11:56:26 -0500

    Créer un fichier de config wct en js

---
+++ wct.conf.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: true,
+  sauce: false,
+  suites: ['test/index.html'],
+  plugins: {
+    sauce: {
+      disabled: true
+    },
+    local: {
+      //disabled: false,
+      remote: false,
+      browsers: ['chrome', 'firefox'],
+      browserOptions: {
+        chrome: ["window-size=1920,1080", "headless", "disable-gpu", "no-sandbox"],
+        firefox: ["--headless"]
+      }
+    }
+  }
+};
