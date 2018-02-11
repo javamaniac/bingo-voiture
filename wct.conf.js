5340db75994b67f56ed6d2262adabb4cdc52c746
Dominic <dominic.stack@gmail.com>
2018-02-11 15:37:34 -0500

    Retire le test de Firefox

---
+++ wct.conf.js
@@ -0,0 +1,25 @@
+module.exports = {
+  verbose: false,
+  sauce: false,
+  suites: [
+    'test/index.html',
+    // 'test/bingo-app.html'
+  ],
+  plugins: {
+    sauce: {
+      disabled: true
+    },
+    local: {
+      //disabled: false,
+      remote: false,
+      browsers: [
+        'chrome',
+        // 'firefox'
+      ],
+      browserOptions: {
+        chrome: ["window-size=1920,1080", "headless", "disable-gpu", "no-sandbox"],
+        // firefox: ["--headless"]
+      }
+    }
+  }
+};
