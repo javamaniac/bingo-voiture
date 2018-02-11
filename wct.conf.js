c4cbcc7ee8d18ae81e8d6360aed8ed061cf178ab
Dominic <dominic.stack@gmail.com>
2018-02-10 22:01:41 -0500

    Ajuster les tests

--- wct.conf.js
+++ wct.conf.js
@@ -1,7 +1,10 @@
 module.exports = {
   verbose: false,
   sauce: false,
-  suites: ['test/index.html'],
+  suites: [
+    'test/index.html',
+    // 'test/bingo-app.html'
+  ],
   plugins: {
     sauce: {
       disabled: true
@@ -9,7 +12,10 @@ module.exports = {
     local: {
       //disabled: false,
       remote: false,
-      browsers: ['chrome', 'firefox'],
+      browsers: [
+        'chrome',
+        'firefox'
+      ],
       browserOptions: {
         chrome: ["window-size=1920,1080", "headless", "disable-gpu", "no-sandbox"],
         firefox: ["--headless"]
